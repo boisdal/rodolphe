7597426b76ba486199abd25e50bff8785d233fa0
Vincent Boisdon <vincent.boisdon@telecom-sudparis.eu>
2021-02-09 19:18:11 +0100

    first working video

--- app/app.js
+++ app/app.js
@@ -1,8 +1,8 @@
-'use strict';
 
 // Declare app level module which depends on views, and core components
 angular.module('myApp', [
   'ngRoute',
+  'ngSanitize',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,14 +1,32 @@
-'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', [
+    'ngRoute',
+    'ngSanitize',
+    "com.2fdevs.videogular",
+  "com.2fdevs.videogular.plugins.controls",
+  "com.2fdevs.videogular.plugins.overlayplay",
+  "com.2fdevs.videogular.plugins.buffering",
+  "com.2fdevs.videogular.plugins.poster"
+])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+    controller: 'View1Ctrl',
   });
 }])
 
-.controller('View1Ctrl', [function() {
-
-}]);
+.controller('View1Ctrl',
+  function ($sce, $scope) {
+      this.config = {
+          preload: "none",
+          sources: [
+              {src: $sce.trustAsResourceUrl("test0.mp4"), type: "video/mp4"}
+          ],
+          theme: {
+              url: "http://www.videogular.com/styles/themes/default/latest/videogular.css"
+          },
+          responsive: true
+      };
+  }
+);
